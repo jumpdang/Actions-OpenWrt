d899d1b817a3f39cd9525212679ac01ea3e31efc
jumpdang <81201093+jumpdang@users.noreply.github.com>
2021-05-05 12:34:50 +0800

    Update x86diy.sh

--- x86diy.sh
+++ x86diy.sh
@@ -4,5 +4,5 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/myf/argon-config
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/myf/jd
 git clone https://github.com/tty228/luci-app-serverchan.git package/myf/serverchan
-git clone https://github.com/vernesong/OpenClash.git package/myf/openclash
+#git clone https://github.com/vernesong/OpenClash.git package/myf/openclash
 ./scripts/feeds update -a && ./scripts/feeds install -a
